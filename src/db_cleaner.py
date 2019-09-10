19223d811df58c5db879846da0a89baa0c82731c
Yann GASCUEL <34003959+lnv42@users.noreply.github.com>
2019-09-10 12:10:04 +0200

    db_cleaner : strip string (rm " "|\n) & remove failed BBCODE

--- src/db_cleaner.py
+++ src/db_cleaner.py
@@ -12,13 +12,29 @@ def clean_db(db, default, languages=[]):
         for field in default:
             if field in row:
                 new_row[field] = row[field]
+                try: # string
+                    new_row[field] = new_row[field].replace("[[/]]","").strip()
+                except:
+                    try: # iterable
+                        for cpt in range(0, len(new_row[field])):
+                            new_row[field][cpt] = new_row[field][cpt].replace("[[/]]","").strip()
+                    except:
+                        pass
             else:
                 new_row[field] = ""
             for lang in languages:
                 if field+lang in row:
                     new_row[field+lang] = row[field+lang]
+                    try: # string
+                        new_row[field+lang] = new_row[field+lang].replace("[[/]]","").strip()
+                    except:
+                        try: # iterable
+                            for cpt in range(0, len(new_row[field+lang])):
+                                new_row[field+lang][cpt] = new_row[field+lang][cpt].replace("[[/]]","").strip()
+                        except:
+                            pass
                     if len(new_row[field]) < 1:
-                        new_row[field] = row[field+lang]
+                        new_row[field] = new_row[field+lang]
         db.insert_record(new_row)
 
 def main(args):
